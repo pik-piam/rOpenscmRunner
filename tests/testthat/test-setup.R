5e3aecc4ef8278118724b7c8c9a565844d2b52b1
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-08-01 17:29:19 +0200

    Formatting for the linter.

--- tests/testthat/test-setup.R
+++ tests/testthat/test-setup.R
@@ -1,12 +1,12 @@
 test_that("setup works for MAGICC", {
   os <- reticulate::import("os")
 
-  setup(magiccExecutable7 = "/some/path")
-  expect_identical(os$environ["MAGICC_EXECUTABLE_7"], "/some/path")
+  setup(magiccExecutable7 = "./some/path")
+  expect_identical(os$environ["MAGICC_EXECUTABLE_7"], "./some/path")
 
   setup(magiccWorkerNumber = 4)
   expect_identical(os$environ["MAGICC_WORKER_NUMBER"], "4")
 
-  setup(magiccWorkerRootDir = "/other/path")
-  expect_identical(os$environ["MAGICC_WORKER_ROOT_DIR"], "/other/path")
+  setup(magiccWorkerRootDir = "./other/path")
+  expect_identical(os$environ["MAGICC_WORKER_ROOT_DIR"], "./other/path")
 })
