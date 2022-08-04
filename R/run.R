aee248dea003198484b20daea7a2b918c02ef1a5
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-08-04 16:35:01 +0200

    Update R/run.R
    Co-authored-by: pfuehrlich-pik <82826417+pfuehrlich-pik@users.noreply.github.com>

--- R/run.R
+++ R/run.R
@@ -46,7 +46,7 @@
 #' # where in MAGICC7 we change "somesetting" and in FaIR we change "fairsetting".
 #' # Also include the changed configuration settings in the output.
 #' run(climateModelsConfigs = list(MAGICC7 = list(list(somesetting = "12"),
-#'                                                  list("somesetting" = "13")),
+#'                                                  list(somesetting = "13")),
 #'                                 "FaIR" = list(list("fairsetting" = "slr"),
 #'                                               list("fairsetting" = "noslr"))),
 #'     scenarios = df,
